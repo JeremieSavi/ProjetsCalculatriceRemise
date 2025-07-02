4bf246498d7e1a290c3c4cc14ad052b107031555
jérémie SAVI <jeremiesavi16@gmail.com>
2025-07-02 11:17:28 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function verifierValeurEntrer(){
     let montantOriginal=Number(document.getElementById('prix_original').value);
     let valeurRemise=Number(document.getElementById('remise').value);
-    if(montantOriginal<0 || valeurRemise<0){
+    if(montantOriginal<=0 || valeurRemise<=0 ){
         paragraphe=document.querySelector('.paragraphe_montant_original');
-        paragraphe.innerHTML="Remplissez correctement les champs ! mettez au moins une valeur"
+        paragraphe.innerHTML="Remplissez correctement les champs !"
+        
     }
 
-};
+
 
 // Sélection de l'espace de résultat 
 let paragraphe_resultat=document.querySelector('.paragraphe_resultat');
